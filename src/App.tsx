c66bbec54ec9f22c4dbb128932cbef22795739e7
Harshit Chadha <harshitchadha@Harshits-MacBook-Pro.local>
2024-05-22 14:24:10 +0530

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="">dd</div>;
-}
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Home = () => {
+  return <div>hello</div>;
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route index path="/" Component={Home} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
